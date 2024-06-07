b298662834deddb147117d51c925a358007dc905
Astro-v <valentin.ph.michel@gmail.com>
2024-06-07 16:27:55 +0200

    fix

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'your secret key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Note: In production, set this to true and ensure you use HTTPS
+}));
 
 const checkCredentials = require('./authentification');
 
 const players = require('./players.json');
 const group_stage = require('./group_stage.json');
+
 
 function checkAuthenticated(req, res, next) {
     console.log(req.sessionID);
@@ -22,16 +33,21 @@ function checkAuthenticated(req, res, next) {
     }
 }
 
+// curl -c cookies.txt -X POST http://localhost:3000/login -H "Content-Type: application/json" -d '{"username":"admin", "password":"admin"}'
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    console.log(req.sessionID);
+
+    console.log('User ' + username + ' is trying to log in');
+    console.log('Password: ' + password);
 
     checkCredentials(username, password).then((isMatch) => {
         if (isMatch) {
             req.session.loggedin = true;
             req.session.username = username;
-            console.log('User ' + username + ' logged in');
-            res.redirect('/directories');
+            // return cookies
+            res.json({ sessionID: req.sessionID });
         } else {
             req.session.loggedin = false;
             console.log('User ' + username + ' failed to log in');
@@ -39,21 +55,30 @@ app.post('/login', (req, res) => {
         }
     }).catch((err) => {
         console.log(err);
-        res.render('login', { wrong_login: true, error: 'An error occurred' });
     });
 });
 
+// curl -b cookies.txt http://localhost:3000/test
 app.get('/poules', (req, res) => {
     res.json(group_stage);
 });
 
-app.post('/poules/:poule_id/:game_id/:result', checkAuthenticated, (req, res) => {
+app.get('/test', checkAuthenticated, (req, res) => {
+    console.log("test");
+});
+
+// curl -b cookies.txt -X POST http://localhost:3000/poules/:0/:1 -H "Content-Type: application/json" -d '{"result":[0,1,2,3,4,5,6,7]}'
+app.post('/poules/:poule_id/:game_id', checkAuthenticated, (req, res) => {
     const poule_id = req.params.poule_id;
     const game_id = req.params.game_id;
-    const result = req.params.result;
+    const result = req.body.result; // tabular 
 
-    players.players[poule_id].score_game[game_id] = 1;
-    res.json({group_stage, players});
+    for (let i = 0; i < 8; i++) {
+        players.players[poule_id].players[i].group_ranking[game_id] = result[i];
+    }
+
+    res.json({ group_stage, players });
+
 });
 
 
