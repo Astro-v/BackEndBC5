2b7671d0c671c02ff271a933755d5d041ddaabf3
Alexandre Janin <alexandrejanin92@gmail.com>
2024-06-19 18:29:26 +0200

    cors update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     console.log(`Headers:${JSON.stringify(req.headers, null, 2)}`);
     console.log(`Params:${JSON.stringify(req.params, null, 2)}`);
     console.log(`Body:${JSON.stringify(req.body, null, 2)}`);
-    res.header('Access-Control-Allow-Origin', 'https://www.brocup.fr'); // Echo back the origin
+    res.header('Access-Control-Allow-Origin', req.headers.origin); // Echo back the origin
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
